caa798eea591b716adf29485c34f532fe1044327
pegeler <paulegeler@gmail.com>
2018-03-20 00:33:24 -0400

    Last-minute changes for March 2018 Rcpp talk

---
+++ Rcpp/hello.cpp
@@ -0,0 +1,10 @@
+// hello.cpp: A Hello World program for C++
+// The next line is a preprocessor directive, kind of like R's `library()`.
+#include <cstdio>
+
+/* This is a multi-line comment. */
+int main (void) 
+{
+  printf("Hello world!\n");
+  return 0;
+}
